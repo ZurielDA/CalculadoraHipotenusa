e1da5d2d613be6de9094d9241fe7d0abd39e97f0
Zuriel Díaz Agustín <zuriel_diaz_agustin@hotmail.com>
2021-10-14 03:09:32 -0500

    Creación de Funcionalidad de la GUI

--- app/src/main/java/com/app/calculadorahipotenusa/MainActivity.kt
+++ app/src/main/java/com/app/calculadorahipotenusa/MainActivity.kt
@@ -4,34 +4,75 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    val limpiarCampos: Button = findViewById(R.id.LimpiarCampos)
-    val calcularHipotenusa: Button = findViewById(R.id.CalcularHipotenusa)
-    val etCatetoC: EditText = findViewById(R.id.EtCatetoC)
-    val etCatetoB: EditText = findViewById(R.id.EtCatetoB)
+    private lateinit var limpiarCampos: Button
+    private lateinit var calcularHipotenusa: Button
+    private lateinit var etCatetoC: EditText
+    private lateinit var etCatetoB: EditText
+    private lateinit var tvResultado: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        inicializarComponentes()
-
+        instanciarComponentes()
+        iniciarComponentes()
     }
 
-    fun inicializarComponentes() {
+    private fun instanciarComponentes(){
+        limpiarCampos = findViewById(R.id.LimpiarCampos)
+        calcularHipotenusa = findViewById(R.id.CalcularHipotenusa)
+        etCatetoC = findViewById(R.id.EtCatetoC)
+        etCatetoB = findViewById(R.id.EtCatetoB)
+        tvResultado = findViewById(R.id.TvResultado)
+    }
+
+    private fun iniciarComponentes() {
         limpiarCampos.setOnClickListener {
             limpiarCampos()
         }
         calcularHipotenusa.setOnClickListener {
-            limpiarCampos()
+
+            var c : String = etCatetoC.getText().toString()
+            var b : String = etCatetoB.getText().toString()
+
+            if(c == "" && b == ""){
+                sendMessage("Favor de llenar los campos!!!")
+
+            }else if (c == ""){
+                sendMessage("Campo de Cateto C vacio!!!")
+
+            }else if(b == ""){
+                sendMessage("Campo de Cateto B vacio!!!")
+
+            }else{
+                var catetoC: Double = validarCampo(c)
+                var catetoB: Double = validarCampo(b)
+
+                var resultado: Double = Math.pow(catetoC,2.0) + Math.pow(catetoB,2.0)
+                tvResultado.setText("La hipotenusa es: ${resultado}")
+
+            }
         }
     }
 
-    fun limpiarCampos(){
-        etCatetoC.setText("")
-        etCatetoB.setText("")
+    private fun validarCampo(valor: String): Double{
+        val valorObtenido = valor.toDouble()
+
+        return valorObtenido
     }
 
+    private fun limpiarCampos(){
+        etCatetoC.setText("")
+        etCatetoB.setText("")
+        tvResultado.setText("")
+    }
+
+    private fun sendMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
