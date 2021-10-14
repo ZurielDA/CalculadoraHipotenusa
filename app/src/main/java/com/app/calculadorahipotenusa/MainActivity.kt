ff9a8cf36c4bc48f17dab24885ae4ec935bcd6a3
Zuriel Díaz Agustín <zuriel_diaz_agustin@hotmail.com>
2021-10-14 01:18:43 -0500

    Creación de GUI

--- app/src/main/java/com/app/calculadorahipotenusa/MainActivity.kt
+++ app/src/main/java/com/app/calculadorahipotenusa/MainActivity.kt
@@ -2,10 +2,36 @@ package com.app.calculadorahipotenusa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    val limpiarCampos: Button = findViewById(R.id.LimpiarCampos)
+    val calcularHipotenusa: Button = findViewById(R.id.CalcularHipotenusa)
+    val etCatetoC: EditText = findViewById(R.id.EtCatetoC)
+    val etCatetoB: EditText = findViewById(R.id.EtCatetoB)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inicializarComponentes()
+
     }
+
+    fun inicializarComponentes() {
+        limpiarCampos.setOnClickListener {
+            limpiarCampos()
+        }
+        calcularHipotenusa.setOnClickListener {
+            limpiarCampos()
+        }
+    }
+
+    fun limpiarCampos(){
+        etCatetoC.setText("")
+        etCatetoB.setText("")
+    }
+
 }
