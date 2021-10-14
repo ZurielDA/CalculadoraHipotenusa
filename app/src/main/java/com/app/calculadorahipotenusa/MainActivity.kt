8a2a9751e9181eb4dfe14b9e9ebfba4d0b0c8b40
Zuriel Díaz Agustín <zuriel_diaz_agustin@hotmail.com>
2021-10-13 23:08:38 -0500

    Añadir elementos de proyectos

---
+++ app/src/main/java/com/app/calculadorahipotenusa/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.app.calculadorahipotenusa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
